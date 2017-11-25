3b934f3fc816ec99c6735147a6e6e06ab49bb2f1
Dio4090 <diogo.rocha42@outlook.com>
2017-11-25 18:04:49 -0200

    Added webservice login logic

--- app/src/main/java/projects/dellectus/myappartment/RegisterActivity.java
+++ app/src/main/java/projects/dellectus/myappartment/RegisterActivity.java
@@ -1,10 +1,20 @@
 package projects.dellectus.myappartment;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -49,7 +59,33 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.btRegister:
-                //All button logic
+                final String name = this.mViewHolder.etName.getText().toString();
+                final String username = this.mViewHolder.etUsername.getText().toString();
+                final String password = this.mViewHolder.etPassword.getText().toString();
+                final int age = Integer.parseInt(this.mViewHolder.etAge.getText().toString());
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if(success) {
+                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                            } else {
+                                // AlertDialog
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(name, username, age, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
                 break;
         }
     }
